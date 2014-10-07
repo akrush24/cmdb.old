6b26ddaaa9d8dd5f103b55629b9b6d14c2ce4b3c
Крушельницкий Андрей Вячеславович <akrushelnitskiy@at-consulting.ru>
2014-10-07 17:06:31 +0400

    edit README

---
+++ query/test.py
@@ -0,0 +1,9 @@
+import mysql.connector
+
+cnx=mysql.connector.connect(user='cmdb',password='unix11',host='cmdb.at-consulting.ru',database='cmdb')
+cursor=cnx.cursor()
+query=('select cmdb.properties.prop_name, cmdb.values.value, cmdb.type.type_name from cmdb.items, cmdb.type, cmdb.properties, cmdb.values where cmdb.values.uuid = cmdb.items.uuid and cmdb.type.type_id = cmdb.items.type_id and cmdb.properties.prop_id = cmdb.values.prop_id')
+cursor.execute(query)
+for row in cursor:
+    print row
+
