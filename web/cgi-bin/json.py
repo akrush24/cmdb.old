0305e85a5682ce38c26085b444a56464b525ae67
Крушельницкий Андрей Вячеславович <akrushelnitskiy@at-consulting.ru>
2014-10-10 11:44:03 +0400

    Fri Oct 10 11:44:03 MSK 2014

---
+++ web/cgi-bin/json.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+print "Content-Type: text/plain;charset=utf-8"
+print 
+
+print '''
+[
+    { 
+        "title":"As important",
+        "url":"URL 1",
+        "categories":"jq",
+        "tags":"jq"
+    },
+    {
+        "title":"Do the best",
+        "url":"URL 2",
+        "categories":"Java",
+        "tags":"java"
+    }
+    {
+        "title":"Do the best",
+        "url":"URL 3",
+        "categories":"Java2",
+        "tags":"java"
+    }
+    {
+        "title":"Do the best",
+        "url":"URL 4",
+        "categories":"Java3",
+        "tags":"java"
+    }
+];
+'''
--- web/cgi-bin/login.py
+++ web/cgi-bin/login.py
@@ -4,39 +4,46 @@
 import cgi
 import mysql.connector
 import calendar, datetime
-import hashlib
+import os
  
 print "Content-Type: text/html;charset=utf-8"
 print ""
 
+# Получение значений, введенных пользователем
 GET = cgi.FieldStorage()
 if 'user' in GET:
 	user = GET["user"].value
 if 'password' in GET:
 	password = GET["password"].value
 
-#user="admin"
-
-mydb=mysql.connector.connect(user='cmdb',password='unix11',host='cmdb.at-consulting.ru',database='cmdb')
-cursor=mydb.cursor()
-query='SELECT `id`,`login`,`pass`,`cookie` FROM `users` WHERE login="'+user+'"'
-
-cursor.execute(query)
-rows = cursor.fetchall()
-#print rows
-
-for row in rows:
-	db_user_id, db_user_login, db_user_pass, db_user_cookie  = row
-
-mydb.close()
-
-h = hashlib.new('ripemd160')
-h.update("Nobody inspects the spammish repetition")
-h.hexdigest()
-
-if user == db_user_login and password == db_user_pass:
-	print "OK"
-	print h
+ 
+try:
+	user, password
+except NameError:
+  print "user or password not defined!"
 else:
-	print "0"
+	
+	mydb=mysql.connector.connect(user='cmdb',password='unix11',host='cmdb.at-consulting.ru',database='cmdb')
+	cursor=mydb.cursor()
+	
+	query='SELECT `id`,`login`,`pass`,`cookie` FROM `users` WHERE login="'+user+'"'
+	cursor.execute(query)
+	rows = cursor.fetchall()
+	for row in rows:
+		db_user_id, db_user_login, db_user_pass, db_user_cookie  = row
 
+	if user == db_user_login and password == db_user_pass:
+		hash = os.urandom(16).encode('hex')
+
+		cursor.execute("UPDATE `cmdb`.`users` SET  `cookie` =  '%s' WHERE `login`='%s';" % (hash, user))
+		mydb.commit();
+	
+	else:
+		print "Authentication Failed!"
+	
+
+	mydb.close()
+	
+
+#print " "
+print "EOF"
