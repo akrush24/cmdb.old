39454a53b7bf475356d707e823dd3f29c448e32c
Крушельницкий Андрей Вячеславович <akrushelnitskiy@at-consulting.ru>
2014-10-17 20:41:44 +0400

    Fri Oct 17 20:41:44 MSK 2014

--- web/cmdb_v0.0.1/static/sendform.js
+++ web/cmdb_v0.0.1/static/sendform.js
@@ -1,7 +1,7 @@
-        // wait for the DOM to be loaded 
+<!--        // wait for the DOM to be loaded 
         $(document).ready(function() { 
             // bind 'myForm' and provide a simple callback function 
             $('#addtypeform').ajaxForm(function() { 
                 alert("Item was added succesfully"); 
             }); 
-        }); 
+        }); -->
